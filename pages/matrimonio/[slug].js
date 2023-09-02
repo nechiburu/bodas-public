39a792037ea24c90039c18e1239e894c34ef45b9
nechiburu <137901882+nechiburu@users.noreply.github.com>
2023-09-02 11:47:18 -0400

    Add files via upload

--- pages/matrimonio/[slug].js
+++ pages/matrimonio/[slug].js
@@ -5,39 +5,40 @@ import PageTitle from '../../components/pagetitle';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/footer';
 import clienteAxios from '../../api/axios';
+import Link from 'next/link';
 
 const MatrimonioPage = (props) => {
-  const router = useRouter();
-  const [team, setTeam] = useState([]);
-  const [projectDetails, setProjectDetails] = useState(null);
+    const router = useRouter();
+    const [team, setTeam] = useState([]);
+    const [projectDetails, setProjectDetails] = useState(null);
 
-  // useEffect para consultar la API
-  useEffect(() => {
-    // query a la api
-    const consultarApi = async () => {
-      try {
-        const teamConsulta = await clienteAxios.get('/parejas');
-        setTeam(teamConsulta.data);
+    // useEffect para consultar la API
+    useEffect(() => {
+        // query a la api
+        const consultarApi = async () => {
+            try {
+                const teamConsulta = await clienteAxios.get('/parejas');
+                setTeam(teamConsulta.data);
 
-        // Buscar el proyecto correspondiente en la lista de team
-        const project = teamConsulta.data.find((item) => item.slug === router.query.slug);
-        setProjectDetails(project);
-      } catch (error) {
-        console.log(error);
-      }
+                // Buscar el proyecto correspondiente en la lista de team
+                const project = teamConsulta.data.find((item) => item.slug === router.query.slug);
+                setProjectDetails(project);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        consultarApi();
+    }, [router.query.slug]);
+
+    const ClickHandler = () => {
+        window.scrollTo(10, 0);
     };
 
-    consultarApi();
-  }, [router.query.slug]);
-
-  const ClickHandler = () => {
-    window.scrollTo(10, 0);
-  };
-
-  if (!projectDetails) {
-    // Puedes mostrar una carga o mensaje mientras se obtienen los datos
-    return <p>Cargando...</p>;
-  }
+    if (!projectDetails) {
+        // Puedes mostrar una carga o mensaje mientras se obtienen los datos
+        return <p>Cargando...</p>;
+    }
 
 
     return (
@@ -62,13 +63,20 @@ const MatrimonioPage = (props) => {
                                     <div className="wpo-portfolio-single-content-des">
                                         <ul>
                                             <li>Ubicación :<span>{projectDetails.location}</span></li>
+                                            <br></br><br></br><br></br>
+                                            <div style={{ textAlign: 'center' }}>
+                                                <Link onClick={ClickHandler} className="theme-btn" href="https://bodas-commerce1-0.vercel.app/">
+                                                    <i className=""></i> Regalar
+                                                </Link>
+                                            </div>
+
                                         </ul>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    
+
                 </div>
             </section>
             <Footer />
