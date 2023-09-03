7222dc134b55f1636ca0aab67ddae83f3c514937
nechiburu <137901882+nechiburu@users.noreply.github.com>
2023-09-03 10:18:06 -0400

    Add files via upload

--- components/about2/index.js
+++ components/about2/index.js
@@ -46,42 +46,45 @@ const About2 = (props) => {
                                     exótica o una celebración tradicional en una encantadora capilla,
                                     estamos preparados para hacerlo realidad.</p>
                                 <h3>Lo que nos hace destacar:</h3>
+                                <h5>Destinos Personalizados: </h5>
                                 <p>Destinos Personalizados: Trabajamos estrechamente contigo para
                                     crear un entorno que se adapte perfectamente a tu estilo y visión.
                                     Cada detalle, desde la decoración hasta la elección del lugar, se
                                     elige cuidadosamente para que tu boda sea un reflejo auténtico de
                                     tu amor.</p>
-                                <p>Experiencias Compartidas: Nada es más convincente que escuchar de
+                                <h5>Experiencias Compartidas:</h5>
+                                <p>Nada es más convincente que escuchar de
                                     aquellos que han vivido la experiencia. Testimonios y relatos de
                                     parejas que ya han confiado en nosotros para su gran día.
                                     Sus historias de éxito son pruebas de nuestro compromiso con
                                     la excelencia.</p>
-                                <p>Regalos Memorables: Sabemos que los pequeños detalles marcan la
+                                <h5>Regalos Memorables: </h5>
+                                <p>Sabemos que los pequeños detalles marcan la
                                     diferencia. Ofrecemos una variedad de opciones para regalos de
                                     invitados que aseguran que cada uno de ellos se lleve un recuerdo
                                     memorable de tu boda. Desde elegantes obsequios personalizados
                                     hasta elementos temáticos que enriquecerán la experiencia de tus
                                     invitados.</p>
-                                <p>Además, en Travelero Weddings creemos que las bodas deben ser 
-                                    experiencias inolvidables desde el primer momento en que comienzan 
-                                    los preparativos. Por eso, ofrecemos un servicio de asesoramiento 
-                                    personalizado desde el primer encuentro. Nuestro equipo de expertos 
-                                    en bodas estará a tu disposición para escuchar tus ideas, entender 
+                                <p>Además, en Travelero Weddings creemos que las bodas deben ser
+                                    experiencias inolvidables desde el primer momento en que comienzan
+                                    los preparativos. Por eso, ofrecemos un servicio de asesoramiento
+                                    personalizado desde el primer encuentro. Nuestro equipo de expertos
+                                    en bodas estará a tu disposición para escuchar tus ideas, entender
                                     tus deseos y convertirlos en un plan de boda a medida.</p>
-                                <p>Estamos aquí para hacer que tus sueños se hagan realidad. 
-                                    Trabajamos en estrecha colaboración contigo para seleccionar 
-                                    el destino perfecto, los detalles más elegantes y cada elemento 
+                                <p>Estamos aquí para hacer que tus sueños se hagan realidad.
+                                    Trabajamos en estrecha colaboración contigo para seleccionar
+                                    el destino perfecto, los detalles más elegantes y cada elemento
                                     que hará que tu boda sea única.</p>
-                                <p>En Travelero Weddings, no solo planificamos bodas, creamos recuerdos 
-                                    inolvidables. Nuestra dedicación a la excelencia y 
-                                    nuestra pasión por lo que hacemos se reflejan en cada 
-                                    celebración que organizamos. Cada pareja que elige confiar 
-                                    en nosotros encuentra una experiencia sin igual, llena de alegría, 
+                                <p>En Travelero Weddings, no solo planificamos bodas, creamos recuerdos
+                                    inolvidables. Nuestra dedicación a la excelencia y
+                                    nuestra pasión por lo que hacemos se reflejan en cada
+                                    celebración que organizamos. Cada pareja que elige confiar
+                                    en nosotros encuentra una experiencia sin igual, llena de alegría,
                                     emoción y amor.</p>
-                                <p>Permítenos ser tu compañero ideal en este viaje hacia tu boda de 
-                                    ensueño. Juntos, haremos que tu día especial sea un verdadero 
-                                    reflejo de tu amor y compromiso, un evento que recordarás con 
-                                    cariño durante toda tu vida. ¿Estás listo para comenzar este 
+                                <p>Permítenos ser tu compañero ideal en este viaje hacia tu boda de
+                                    ensueño. Juntos, haremos que tu día especial sea un verdadero
+                                    reflejo de tu amor y compromiso, un evento que recordarás con
+                                    cariño durante toda tu vida. ¿Estás listo para comenzar este
                                     emocionante viaje con nosotros?</p>
                             </div>
                         </div>
