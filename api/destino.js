ae3a8d5337f5e2058c35f48a72337ed16e2736d6
nechiburu <137901882+nechiburu@users.noreply.github.com>
2023-09-02 21:46:33 -0400

    Add files via upload

---
+++ api/destino.js
@@ -0,0 +1,64 @@
+import simg1 from '/public/images/service/playa.webp'
+import simg2 from '/public/images/service/europa.jpeg'
+import simg3 from '/public/images/service/turquia.jpeg'
+import simg4 from '/public/images/service/Riviera-Maya.jpg'
+import simg5 from '/public/images/service/Tulum.jpg'
+import simg6 from '/public/images/service/Icono.png'
+
+import sinnermg1 from '/public/images/service-single/2.jpg'
+import sinnermg2 from '/public/images/service-single/3.jpg'
+
+import canmg1 from '/public/images/service-single/c1.jpg'
+import canmg2 from '/public/images/service-single/c3.jpg'
+//import canmg3 from '/public/images/service-single/c3.jpg'
+
+import jamg1 from '/public/images/service-single/j1.jpg'
+import jamg2 from '/public/images/service-single/j2.jpg'
+// import jamg3 from '/public/images/service-single/j3.jpg'
+
+import pcmg1 from '/public/images/service-single/pc1.jpg'
+import pcmg2 from '/public/images/service-single/pc2.jpg'
+// import pcmg3 from '/public/images/service-single/pc3.jpg'
+
+import rmmg1 from '/public/images/service-single/rm1.jpg'
+import rmmg2 from '/public/images/service-single/rm2.jpg'
+// import rmmg3 from '/public/images/service-single/rm3.jpg'
+
+import tmg1 from '/public/images/service-single/t3.jpg'
+import tmg2 from '/public/images/service-single/t2.jpg'
+// import tmg3 from '/public/images/service-single/t3.jpg'
+
+import singleImg1 from '/public/images/service-single/1.jpg'
+import singleImg2 from '/public/images/service-single/4.jpg'
+import singleImg3 from '/public/images/service-single/5.jpg'
+import singleImg4 from '/public/images/service-single/6.jpg'
+import singleImg5 from '/public/images/service-single/7.jpg'
+import singleImg6 from '/public/images/service-single/8.jpg'
+
+export const Destinos = [
+    
+    {
+        id: '1',
+        fIcon1:simg6,
+        title:'Caribe',
+        slug: 'Caribe',
+        simg1:simg1,
+        // sinnerImg3:canmg3,
+    },
+    {
+        id: '2',
+        fIcon1:simg6,
+        title:'Europa',
+        slug: 'Europa',
+        simg1:simg2,
+        // sinnerImg3:jamg3,
+    },
+    {
+        id: '3',
+        fIcon1:simg6,
+        title:'Turquia',
+        slug: 'Turquia',
+        simg1:simg3,
+        // sinnerImg3:pcmg3,
+    },
+]
